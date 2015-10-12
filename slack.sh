436b20124d18795f1322ded9e62f477e3b315f1f
koitatu <koitatu@gmail.com>
2015-10-13 00:28:55 +0900

    add slack.sh

---
+++ slack.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CHANNEL="slackのチャンネル名"
+WEBHOOK_URL="slackのwebhookURL"
+
+status="$1"
+
+if [ "$status" == 'OK' ]; then
+        emoji=':smile:'
+elif [ "$status" == 'PROBLEM' -o "$status" == 'NG' ]; then
+        emoji=':frowning:'
+else
+        emoji=':ghost:'
+fi
+
+message="$@"
+payload="payload={\"channel\": \"#koizumi_zabbix_aws\", \"username\": \"zabbix_bot_chan\", \"text\": \"${message}\", \"icon_emoji\": \"${emoji}\"}"
+
+curl -m 5 --data "${payload}" ${WEBHOOK_URL}
